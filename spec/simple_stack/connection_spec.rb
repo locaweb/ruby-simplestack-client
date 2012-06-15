f16491fe9137fbe102114c97c259792808b02f47
Thiago Morello <thiago.morello@locaweb.com.br>
2012-06-15 14:05:16 -0300

    Adding tests

---
+++ spec/simple_stack/connection_spec.rb
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+require "spec_helper"
+
+describe SimpleStack::Connection do
+  let(:url) {"http://simplestack.server"}
+  subject {SimpleStack::Connection.new :url => url}
+  
+  it "should keep the connection url" do
+    subject.url.should == url
+  end
+  
+  it "should connect to an hypervisor" do
+    host = "hypervisor_host"
+    hypervisor = subject.connect_to :mock, host: host
+    hypervisor.type.should == :mock
+    hypervisor.host.should == host
+    hypervisor.url.should == "#{url}/mock/#{host}"
+  end
+end
