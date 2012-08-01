b916ad55ee51bb4c74b12af98b42f44b9749f782
Thiago Morello <thiago.morello@locaweb.com.br>
2012-08-01 10:39:56 -0300

    Addoing host

---
+++ lib/simple_stack/host.rb
@@ -0,0 +1,4 @@
+module SimpleStack
+  class Host < Entity
+  end
+end
--- lib/simple_stack/hypervisor.rb
+++ lib/simple_stack/hypervisor.rb
@@ -42,6 +42,10 @@ module SimpleStack
       cached_attributes[:info] ||= self.get url
     end
 
+    def hosts
+      cached_attributes[:hosts] ||= SimpleStack::Collection.new self, self, "#{url}/hosts", SimpleStack::Host
+    end
+
     def storages
       cached_attributes[:storages] ||= SimpleStack::Collection.new self, self, "#{url}/storages", SimpleStack::Storage
     end
